3ef4f0239bdfed9f44ed4b7a1a6e65f7d0c9f1f8
Rahul Jain <rahulmjain24@outlook.com>
2022-01-02 11:14:26 +0530

    fixed the app

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 const cityURL = 'http://dataservice.accuweather.com/locations/v1/topcities/150?apikey=';
-const key = 'VNFYJYsX5nDehyh67wuSJb5GAomsJAx2';
+const key = 'xNH9a9gqiaLJbUG1Rm5sAWIOtXHiLlbS';
 const weatherURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
 
 exports.getCity = async (cityname = 'tokyo', res) => {
     try {
         const response = await axios.get(cityURL + key);
-        const thecity = response.data.filter(d => d.EnglishName.toLowerCase() === cityname);
+        const thecity = response.data.filter(d => d.EnglishName.toLowerCase() === cityname.toLowerCase());
         console.log(thecity[0])
 
         if (thecity.length === 0) {
@@ -15,12 +15,6 @@ exports.getCity = async (cityname = 'tokyo', res) => {
         } else {
            const response2 = await axios.get(weatherURL + thecity[0].Key + '?apikey=' + key);
             const data = response2.data[0];
-            const dummy = {
-                WeatherText: 'Clear',
-                Temperature: {
-                    Metric: 20, Unit: 'C'
-                }
-            }
             res([data,thecity[0]])
         }
     } catch (e) {

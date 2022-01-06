d87133a52325512b53968db7c4c894aebdefe644
Rahul Jain <rahulmjain24@outlook.com>
2022-01-06 14:57:47 +0530

    web fix

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 
-const cityURL = 'http://dataservice.accuweather.com/locations/v1/topcities/150?apikey=';
+const cityURL = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=';
 const key = 'xNH9a9gqiaLJbUG1Rm5sAWIOtXHiLlbS';
 const weatherURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
 
 exports.getCity = async (cityname = 'tokyo', res) => {
     try {
-        const response = await axios.get(cityURL + key);
-        const thecity = response.data.filter(d => d.EnglishName.toLowerCase() === cityname.toLowerCase());
-        console.log(thecity[0])
+        const response = await axios.get(cityURL + key+"&q="+cityname);
+        const thecity = response.data[0]
+        console.log(thecity)
 
         if (thecity.length === 0) {
             console.log('Please enter a valid location');
         } else {
-           const response2 = await axios.get(weatherURL + thecity[0].Key + '?apikey=' + key);
+           const response2 = await axios.get(weatherURL + thecity.Key + '?apikey=' + key);
             const data = response2.data[0];
-            res([data,thecity[0]])
+            res([data,thecity])
         }
     } catch (e) {
         console.log(e);

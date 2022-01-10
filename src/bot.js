f55d6f2ff66201fbee1ae97c8cdb7ec9c111509d
Rahul Jain <rahulmjain24@outlook.com>
2022-01-10 16:18:38 +0530

    sad

--- src/bot.js
+++ src/bot.js
@@ -42,20 +42,6 @@ client.on('message', async (msg) => {
     if(msg.content.includes('!w')){        
         const cityname = msg.content.slice(3).trim().toLowerCase()
         const rep = await getForcast(cityname)
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(`The temperature in ${rep.name} is ${rep.Temperature.Metric.Value}°C`)
-            .setURL(rep.link)
-            .setAuthor({ name: msg.author.username, iconURL: `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.webp?size=80`})
-            .addFields(
-                { name: 'Country', value: rep.country },
-                { name: 'Current time', value: rep.time.slice(11,16)+" GMT+"+rep.time.slice(20), inline: true },
-                { name: 'Current date', value: rep.time.slice(0,10), inline: true },
-            )
-            .setImage(imgURL)
-            .setThumbnail(`https://rahul-jain-web-server.herokuapp.com/img/iconspng/${rep.weatherIcon}.png`)
-            .setTimestamp()
-            .setFooter({ text: `Weather bot by Rahul`, iconURL: 'https://cdn.discordapp.com/avatars/359259183440199681/96b992bbf3107f1d4f6d4be22f5d225b.webp?size=80' });
         msg.reply(`The temperature in ${rep.name} is ${rep.Temperature.Metric.Value}°C`)
       //  msg.channel.send(exampleEmbed)
     }

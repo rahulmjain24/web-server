36173d8b97f45307c256e5cff044b6acae9707d5
Rahul Jain <rahulmjain24@outlook.com>
2022-01-10 16:02:52 +0530

    reverting back

--- src/bot.js
+++ src/bot.js
@@ -10,6 +10,7 @@ const getForcast = async (cityname = 'tokyo', res) => {
     try {
         const response = await axios.get(cityURL + key + "&q=" + cityname);
         const thecity = response.data[0]
+        //console.log(thecity)
 
         if (thecity.length === 0) {
             console.log('Please enter a valid location');
@@ -17,10 +18,16 @@ const getForcast = async (cityname = 'tokyo', res) => {
             const response2 = await axios.get(weatherURL + thecity.Key + '?apikey=' + key);
             const data = response2.data[0];
             let newData = {
+                condition: data.WeatherText,
+                weatherIcon: data.WeatherIcon,
+                Temperature: { ...data.Temperature },
+                link: data.Link,
                 name: thecity.EnglishName,
-                temp: data.Temperature.Metric.Value
+                country: thecity.Country.LocalizedName,
+                isDayTime: data.IsDayTime,
+                time: data.LocalObservationDateTime
             }
-            //console.log(newData)
+            console.log(data)
             return newData
         }
     } catch (e) {
@@ -35,7 +42,22 @@ client.on('message', async (msg) => {
     if(msg.content.includes('!w')){        
         const cityname = msg.content.slice(3).trim().toLowerCase()
         const rep = await getForcast(cityname)
-        msg.reply(`The temperature in ${rep.name} is ${rep.temp}°C`)
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`The temperature in ${rep.name} is ${rep.Temperature.Metric.Value}°C`)
+            .setURL(rep.link)
+            .setAuthor({ name: msg.author.username, iconURL: `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.webp?size=80`})
+            .addFields(
+                { name: 'Country', value: rep.country },
+                { name: 'Current time', value: rep.time.slice(11,16)+" GMT+"+rep.time.slice(20), inline: true },
+                { name: 'Current date', value: rep.time.slice(0,10), inline: true },
+            )
+            .setImage(imgURL)
+            .setThumbnail(`https://rahul-jain-web-server.herokuapp.com/img/iconspng/${rep.weatherIcon}.png`)
+            .setTimestamp()
+            .setFooter({ text: `Weather bot by Rahul`, iconURL: 'https://cdn.discordapp.com/avatars/359259183440199681/96b992bbf3107f1d4f6d4be22f5d225b.webp?size=80' });
+        // msg.reply(`The temperature in ${rep.name} is ${rep.temp}°C`)
+        message.channel.send(exampleEmbed)
     }
 })
 

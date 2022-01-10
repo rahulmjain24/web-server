2303dbf307cd2b4d0c697f8565cb2668193f91d7
Rahul Jain <rahulmjain24@outlook.com>
2022-01-10 16:13:55 +0530

    sad

--- src/bot.js
+++ src/bot.js
@@ -57,7 +57,7 @@ client.on('message', async (msg) => {
             .setTimestamp()
             .setFooter({ text: `Weather bot by Rahul`, iconURL: 'https://cdn.discordapp.com/avatars/359259183440199681/96b992bbf3107f1d4f6d4be22f5d225b.webp?size=80' });
         msg.reply(`The temperature in ${rep.name} is ${rep.Temperature.Metric.Value}°C`)
-        msg.channel.send(exampleEmbed)
+      //  msg.channel.send(exampleEmbed)
     }
 })
 
